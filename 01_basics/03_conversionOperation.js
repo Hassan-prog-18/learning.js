85eae3c74d922e4a9841f8347fd84bf6e4f107d7
Hassan-prog-18 <hassanprogrammer11@gmail.com>
2025-09-25 15:22:16 +0000

    Changes done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "753"
+
+// console.log(typeof score); 
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+let IsLoggedIn = "0"
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 453
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
