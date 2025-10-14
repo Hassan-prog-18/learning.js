fc7a562c1432f53107e37ea9df1ea7f491f9dd1b
Hassan-prog-18 <hassanprogrammer11@gmail.com>
2025-10-14 14:01:35 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,4 +42,37 @@ return
 }
 return `${username} just logged in`
 }
-console.log(loginUserMessage("Chico"));
+// console.log(loginUserMessage("Chico"));
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(50,1000,150,780));
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calculateCartPrice(50,1000,150,780));
+
+const user = {
+    username : "Hassan",
+    price: "399"
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price : "499"
+})
+
+const myNewArray = [300, 4000, 500, 2000 ]
+
+function returnSecondValue(getArray){
+return getArray[3]
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,700, 230, 85000]));
+
