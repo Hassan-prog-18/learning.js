36f9dc310a91091fd0ebc8aa8a7d6654ce643f25
Hassan-prog-18 <hassanprogrammer11@gmail.com>
2025-10-17 14:45:20 +0000

    one.js control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,46 @@
+// if.......
+
+// <, >, <=, >=, !, !=, ==, ===, !==
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if (temperature < 50) {
+//     console.log(`less than 50`);
+// }else{
+//     console.log(`temperature is greater than 50`);
+// }
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+// const balance = 1000
+// if (balance < 500) {
+//     console.log("less than 500")
+// } else if (balance < 750){
+//     console.log("less than 750")    
+// }else if (balance < 900){
+//     console.log("less than 900")    
+// }
+// else { (balance < 1200)
+//     console.log("less than 1200") 
+//   }
+
+// const userLoggedIn = true
+// const debitCard = true
+// if (userLoggedIn && debitCard) {
+//     console.log("Allow to buy course");
+// }
+
+// const loggedInFromGoogle = true
+// const loggedInFromEmail = false
+// if (loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged In");
+// }
+
+
+
