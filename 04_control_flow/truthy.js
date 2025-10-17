a2e2df02c747bc774fff69bf9010c8a2ca7f8ef2
Hassan-prog-18 <hassanprogrammer11@gmail.com>
2025-10-17 14:46:20 +0000

    truthy js control flow

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,57 @@
+// const userEmail = "Hassan.Ai"
+// if (userEmail) {
+//     console.log("Got user email");
+// }else{
+//     console.log("don't have user email");
+    
+// }
+
+// const userEmail2 = ""
+// if (userEmail2) {
+//     console.log("Got user email");
+// }else{
+//     console.log("don't have user email");
+    
+// }
+
+// const userEmail3 = []
+// if (userEmail3) {
+//     console.log("Got user email");
+// }else{
+//     console.log("don't have user email");
+    
+// }
+
+// FALSY VALUES
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// TRUTHY VALUES
+// "0", "False", " ", [], {}, function(){}
+
+// const userEmail5 = []
+// if (userEmail5.length === 0) {
+//     console.log("Array is empty");
+// }
+
+// const emptyObj = {}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+    
+// }
+
+// Nullish Coalescing Operator (??): null undefined
+
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 20 ?? 30
+
+// console.log(val1);
+
+// Terniary Operator...
+
+// condition ? true : false
+
+// const iceTeaPrice = 100
+// iceTeaPrice <= 80 ? console.log("less than 80") : console.log("Greater than 80");
